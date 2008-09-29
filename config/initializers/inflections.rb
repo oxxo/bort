1a23c40afb818336b0175614ebf0590c08c27bf2
Carlos Kozuszko <carlos@insignia4u.com>
2008-09-29 00:59:00 -0300

    Adding spanish inflections to initializers/inflections.rb as comments

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,24 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+# Spanish inflections
+
+# Add new inflection rules using the following format 
+# (all these examples are active by default):
+#ActiveSupport::Inflector.inflections do |inflect|
+#  inflect.clear :singulars
+#  inflect.plural( /([aeiou])$/i, '\1s' )
+#  inflect.plural( /(d|l|n|r)$/i, '\1es' )
+#  inflect.plural( /(m)$/i, '\1s' )
+#  inflect.plural( /^(user)$/i, '\1s')
+#  inflect.plural( /^(session)$/i, '\1s')
+#  inflect.plural( /^(role)$/i, '\1s')
+#
+#  inflect.singular( /([aeiou])s$/i, '\1' )
+#  inflect.singular( /(d|l|n|r)es$/i, '\1')
+#  inflect.singular( /(m)s$/i, '\1')
+#  inflect.singular( /^(user)s$/i, '\1')
+#  inflect.singular( /^(session)s$/i, '\1')
+#  inflect.singular( /^(role)s$/i, '\1')
+#end
