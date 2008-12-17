2bc1d40f9750adefe4e60955dd7ecb7e07cb0335
Carlos Kozuszko <carlos@insignia4u.com>
2008-12-17 15:49:07 -0200

    deleting administrate_me

--- vendor/plugins/administrate_me/files/initializers/administrate_me.rb
+++
@@ -1,5 +0,0 @@
-I18n.load_path << 'vendor/plugins/administrate_me/locale/sp-AR.rb'
-I18n.load_path << 'vendor/plugins/administrate_me/locale/en-US.rb'
-
-# Don't forget to set your default locale on environment.rb with this line:
-# config.i18n.default_locale = :sp
