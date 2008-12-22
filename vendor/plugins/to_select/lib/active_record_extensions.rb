d7d1923551311f46a3326383429ec3530c83a81c
Maria E. Chacon <eugeniach@insignia4u.com>
2008-12-22 16:45:43 -0200

    Updating to_select plugin

--- vendor/plugins/to_select/lib/active_record_extensions.rb
+++ vendor/plugins/to_select/lib/active_record_extensions.rb
@@ -1,7 +1,7 @@
 module ToSelect
   module ActiveRecordExtensions
-    def to_select(text_method=:name, conditions=nil)
-      find(:all, :conditions => conditions).to_select(text_method)
+    def to_select(text_method=:name, options = {})
+      find(:all, :conditions => options[:conditions]).to_select(text_method, options)
     end
   end
 end
