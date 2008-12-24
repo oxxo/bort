50ec469ae331bcf9efce2985d391c1dda221de03
Carlos Kozuszko <carlos@insignia4u.com>
2008-12-24 13:26:24 -0200

    Adding spanish translations. And setting spanish as the default language.

--- config/environment.rb
+++ config/environment.rb
@@ -76,5 +76,5 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   config.active_record.observers = :user_observer
   
-  config.i18n.default_locale = :en
+  config.i18n.default_locale = :es
 end
